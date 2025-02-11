7e29888beb4c432e6387ed9526a8da4cb91cbae1
ThaysMariano <thaysdasilvamariano@gmail.com>
2025-02-11 19:57:48 -0300

    enum, valores como int

--- tiposGenericos/app/src/main/java/ads/poo/ValoresCartas.java
+++ tiposGenericos/app/src/main/java/ads/poo/ValoresCartas.java
@@ -2,23 +2,23 @@ package ads.poo;
 
 public enum ValoresCartas {
 
-    AS("1"),
-    DOIS("2"),
-    TRES("3"),
-    QUATRO("4"),
-    CINCO("5"),
-    SEIS("6"),
-    SETE("7"),
-    OITO("8"),
-    NOVE("9"),
-    DEZ("10"),
-    VALETE("J"),
-    DAMA("Q"),
-    REI("K");
+    AS(1),
+    DOIS(2),
+    TRES(3),
+    QUATRO(4),
+    CINCO(5),
+    SEIS(6),
+    SETE(7),
+    OITO(8),
+    NOVE(9),
+    DEZ(10),
+    VALETE(11),
+    DAMA(12),
+    REI(13);
 
-    public String valor;
+    public int valor;
 
-    ValoresCartas(String valor) {
+    ValoresCartas(int valor) {
         this.valor = valor;
     }
 
